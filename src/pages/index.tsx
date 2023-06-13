10c8bfc356d11147d159920071a46cb2a0e7376a
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-13 16:00:09 +0600

    todo created showed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
 
 const Home: NextPage = () => {
   const [todos, setTodos] = useState<Todo[]>([
@@ -9,11 +9,43 @@ const Home: NextPage = () => {
       isCompleted: false
     }
   ]);
+
   const [title, setTitle] = useState("");
 
+  //create todo function
+  const createTodo = (e: (FormEvent<HTMLFormElement | HTMLButtonElement>)) => {
+    e.preventDefault();
+    setTodos((prevTodos) => [...prevTodos, {
+      id: todos.length + 1,
+      title,
+      isCompleted: false
+    }]
+    )
+    setTitle("");
+  };
+
   return (
-    <section>
-      <div></div>
+    <section className="p-10">
+      <form onSubmit={(e) => createTodo(e)}>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          className="bg-gray-300 rounded-md p-2"
+          onChange={(e) => setTitle(e.target.value)} />
+        <button type="submit" onSubmit={(e) => createTodo(e)}>Add</button>
+      </form>
+      <div>
+        {
+          todos && todos.map((todo) => (
+            <div key={todo.id} className="flex items-center p-2">
+              <h1 className={`text-2xl font-bold ${todo.isCompleted ? "line-through" : ""}`}>
+                {todo.title}
+              </h1>
+            </div>
+          ))
+        }
+      </div>
     </section>
   )
 };
