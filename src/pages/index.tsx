1447e5746ebc0eac9eeb4fabf13754756ae30cb6
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-13 16:22:03 +0600

    custom toggle button added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,10 +44,15 @@ const Home: NextPage = () => {
         {
           todos && todos.map((todo) => (
             <div key={todo.id} className="flex items-center p-2">
-              <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={() => changeStatus(todo.id)} />
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="sr-only peer"
+                  checked={todo.isCompleted}
+                  onChange={() => changeStatus(todo.id)} />
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" ></div>
+                <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+              </label>
               <h1 className={`text-2xl font-bold ${todo.isCompleted ? "line-through" : ""}`}>
                 {todo.title}
               </h1>
