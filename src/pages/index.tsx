ea2d64519074d000c4f6a3b3ee94f21b96586e26
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-13 15:41:22 +0600

    initial States

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { type NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  return <div></div>
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      id: 0,
+      title: "title 1",
+      isCompleted: false
+    }
+  ]);
+  const [title, setTitle] = useState("");
+
+  return (
+    <section>
+      <div></div>
+    </section>
+  )
 };
 
 export default Home;
