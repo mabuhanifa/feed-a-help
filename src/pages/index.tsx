6a2eac5a3a62c83bf82b83dc06bc00f7e5bf8f96
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-15 15:27:30 +0600

    checkbox separated as a component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import { useState, type FormEvent } from "react";
+import CheckBox from "~/components/CheckBox";
 
 const Home: NextPage = () => {
   const [todos, setTodos] = useState<Todo[]>([
@@ -44,15 +45,7 @@ const Home: NextPage = () => {
         {
           todos && todos.map((todo) => (
             <div key={todo.id} className="flex items-center p-2">
-              <label className="relative inline-flex items-center cursor-pointer">
-                <input
-                  type="checkbox"
-                  className="sr-only peer"
-                  checked={todo.isCompleted}
-                  onChange={() => changeStatus(todo.id)} />
-                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" ></div>
-                <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
-              </label>
+              <CheckBox isCompleted={todo.isCompleted} changeStatus={changeStatus} id={todo.id} />
               <h1 className={`text-2xl font-bold ${todo.isCompleted ? "line-through" : ""}`}>
                 {todo.title}
               </h1>
