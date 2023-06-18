4b17bc887897e1d140a62b6b451404ccc3f6433f
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-18 20:55:48 +0600

    Todos separated from home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,12 @@
 import { type NextPage } from "next";
-import { useState, type FormEvent } from "react";
-import { useDispatch, useSelector } from "~/contextAPI/hooks";
+import Todos from "~/components/Todos";
 
 const Home: NextPage = () => {
-  const dispatch = useDispatch();
-  const todos = useSelector((s) => s.todos);
 
-  const [title, setTitle] = useState("");
-
-  //create todo function
-
-  const createTodo = (e: (FormEvent<HTMLFormElement | HTMLButtonElement>)) => {
-    e.preventDefault();
-    dispatch({
-      type: "ADD_TODO",
-      payload:
-      {
-        id: todos.length + 1,
-        title,
-        isCompleted: false
-      }
-    });
-    setTitle("");
-  };
-
-  // const changeStatus = (id: number) => {
-  //   setTodos((prevTodos) => prevTodos.map((prevTodo) => prevTodo.id === id ?
-  //     { ...prevTodo, isCompleted: !prevTodo.isCompleted }
-  //     :
-  //     prevTodo))
-  // }
   return (
-    <section className="p-10">
-      <form onSubmit={(e) => createTodo(e)}>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          className="bg-gray-300 rounded-md p-2"
-          onChange={(e) => setTitle(e.target.value)} />
-        <button type="submit" onSubmit={(e) => createTodo(e)}>Add</button>
-      </form>
-      <div>
-        {
-          todos && todos.map((todo) => (
-            <div key={todo.id} className="flex items-center p-2">
-              {/* <CheckBox id={todo.id} changeStatus={changeStatus} isCompleted={todo.isCompleted} /> */}
-              <h1 className={`text-2xl font-bold ${todo.isCompleted ? "line-through" : ""}`}>
-                {todo.title}
-              </h1>
-            </div>
-          ))
-        }
-      </div>
-    </section>
+    <>
+      <Todos />
+    </>
   )
 };
 
