ffd82f9d0d315f7a053ce1758d15022ea9e581ec
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-22 10:23:58 +0600

    updateTitle functionality with improved ui

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,30 +1,78 @@
+import { useState, type FormEvent } from "react";
+import { BiSave } from "react-icons/bi";
+import { FiEdit } from "react-icons/fi";
 import { VscTrash } from "react-icons/vsc";
 import { useDispatch } from "~/contextAPI/hooks";
 import CheckBox from "./CheckBox";
 
 export default function Todo({ todo }: { todo: Todo }) {
   const dispatch = useDispatch();
+  const [title, setTitle] = useState(todo.title);
+  const [edit, setEdit] = useState(false);
+
+  const updateTitle = (e: FormEvent<HTMLFormElement | HTMLButtonElement>) => {
+    e.preventDefault();
+    if (title.trim() === "") {
+      return;
+    }
+    if (title === todo.title) {
+      return;
+    }
+    dispatch({
+      type: "CHANGE_TITLE",
+      payload: {
+        id: todo.id,
+        title: title,
+      },
+    });
+    setEdit(false);
+  };
   return (
-    <div key={todo.id} className="flex items-center p-2">
-      <CheckBox key={todo.id} todo={todo} />
-      <h1
-        className={`text-2xl font-bold ${
-          todo.isCompleted ? "line-through" : ""
-        }`}
-      >
-        {todo.title}
-      </h1>
-      <button
-        className="col-span-2 flex items-center justify-center text-red-600"
-        onClick={() => {
-          dispatch({
-            type: "REMOVE_TODO",
-            payload: todo.id,
-          });
-        }}
-      >
-        <VscTrash size={20} />
-      </button>
+    <div className="shad my-2 grid grid-cols-12 rounded  p-4">
+      <div className="col-span-2 flex items-center justify-center">
+        <CheckBox todo={todo} />
+      </div>
+      <div className="col-span-8">
+        <form onSubmit={(e) => updateTitle(e)} className="flex items-center">
+          <input
+            type="text"
+            name="title"
+            value={title}
+            disabled={!edit}
+            className={`mr-2 rounded px-3 py-1 text-xl font-bold md:mr-10 ${
+              todo.isCompleted ? "line-through" : ""
+            }  ${edit ? "bg-gray-300" : ""}`}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={(e) => {
+              setEdit((m) => !m);
+              updateTitle(e);
+            }}
+            className="text-green-600"
+          >
+            {edit ? (
+              <BiSave size={20} className="mt-1" />
+            ) : (
+              <FiEdit size={20} className="mt-1" />
+            )}
+          </button>
+        </form>
+      </div>
+      <div className="col-span-2 flex items-center justify-center gap-x-5">
+        <button
+          className="flex items-center justify-center text-red-600 "
+          onClick={() => {
+            dispatch({
+              type: "REMOVE_TODO",
+              payload: todo.id,
+            });
+          }}
+        >
+          <VscTrash size={20} />
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,5 @@
 import { useState, type FormEvent } from "react";
+import { VscAdd } from "react-icons/vsc";
 import { useDispatch, useSelector } from "~/contextAPI/hooks";
 import Todo from "./Todo";
 
@@ -22,21 +23,32 @@ export default function Todos() {
     setTitle("");
   };
   return (
-    <section className="p-10">
-      <form onSubmit={(e) => createTodo(e)}>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          className="rounded-md bg-gray-300 p-2"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <button type="submit" onSubmit={(e) => createTodo(e)}>
-          Add
-        </button>
-      </form>
-      <div>
-        {todos && todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
+    <section className="p-2 py-10 md:p-10 ">
+      <div className="flex justify-center">
+        <form
+          onSubmit={(e) => createTodo(e)}
+          className="flex items-center gap-2"
+        >
+          <input
+            type="text"
+            name="title"
+            value={title}
+            className="rounded-md bg-gray-300 p-2 placeholder:text-gray-600"
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Add a todo"
+          />
+          <button
+            type="submit"
+            onSubmit={(e) => createTodo(e)}
+            className="rounded-md bg-blue-500 p-2.5 text-white"
+          >
+            <VscAdd size={20} />
+          </button>
+        </form>
+      </div>
+
+      <div className="mt-10 flex flex-col items-center justify-center">
+        {todos && todos.map((todo) => <Todo todo={todo} key={todo.id} />)}
       </div>
     </section>
   );
